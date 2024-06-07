aa93f1ffddc587954e3e7f4f7b99edb9b067c781
TheRABU <mdfazlerabbi114@gmail.com>
2024-06-07 22:18:50 +0600

    installed jwt and created auth related api

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
+const jwt = require("jsonwebtoken");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -35,6 +36,14 @@ async function run() {
       .db("nos-car-services")
       .collection("bookings");
 
+    // AUTH related api
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      console.log(user);
+      res.send(user);
+    });
+
+    // SERVICES Related apis
     app.get("/services", async (req, res) => {
       const cursor = serviceCollection.find();
       const result = await cursor.toArray();
@@ -53,7 +62,7 @@ async function run() {
       res.send(result);
     });
 
-    // bookings
+    // bookings related apis
 
     app.get("/bookings", async (req, res) => {
       let query = {};
